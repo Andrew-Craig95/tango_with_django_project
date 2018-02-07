2e1fb906f04086066d14bb793bbc3cb9fe8cf5c9
Andrew Craig <2328324c@student.gla.ac.uk>
2018-02-07 10:27:32 +0000

    second commit

--- rango/views.py
+++ rango/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Rango says hey there partner!")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page.")
